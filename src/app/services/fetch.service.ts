89c82ba22b08679a39e9c2006c7f869d71fe62c9
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-19 14:36:12 +0200

    Routed the service to an API

--- src/app/services/fetch.service.ts
+++ src/app/services/fetch.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchImage() {
+
+    return this.http.get<any>("https://dog.ceo/api/breeds/image/random");
+
+  }
+
 }
