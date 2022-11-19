9a55d9126b86aa27edd3b903a43ef813f0ebfadd
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-19 15:47:44 +0200

    Fixed some bugs

--- src/app/predict/predict.page.ts
+++ src/app/predict/predict.page.ts
@@ -1,6 +1,7 @@
 import { PredictionService } from './../services/prediction.service';
 import { Component } from '@angular/core';
 import { PredictionModel } from '../models/prediction.model';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-predict',
@@ -9,11 +10,16 @@ import { PredictionModel } from '../models/prediction.model';
 })
 export class PredictPage {
 
-  available_prediction: Boolean = false;
+  temp_name: String = "";
   prediction: PredictionModel = { age: 0, gender: "", name: "", nationality: []};
 
   constructor(private predictionService: PredictionService) { }
 
+  emptyPrediction() : Boolean {
+
+    return this.prediction.age == 0 || this.prediction.name == "" || this.prediction.gender == "" || this.prediction.nationality.length == 0;
+
+  }
 
   get name(): String {
 
@@ -47,7 +53,10 @@ export class PredictPage {
 
   predict() {
 
-    this.available_prediction = true;
+    this.prediction.age = 0;
+    this.prediction.gender = "";
+    this.prediction.nationality = [];
+    this.prediction.name = this.temp_name;
     this.predictionService.build(this.prediction);
 
   }
